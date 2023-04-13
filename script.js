bbb10ea035be483af7712bd958d07e1e0736f772
Subir Kumar <118863821+subirKumarPratihar@users.noreply.github.com>
2023-04-13 17:10:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("I am in git.");
+console.log("hello world!");
+
+const count = document.querySelector(".count");
+
+const mrp = document.querySelector(".mrp");
+
+const discountValue =document.querySelector(".discount-value");
+
+const discountNo = Number.parseFloat(discountValue.innerHTML.replace(/[^\d\.]*/g, ''));
+
+const mrpNo = Number.parseFloat(mrp.innerHTML.replace(/[^\d\.]*/g, ''));
+
+const sellPrice = document.querySelector(".sell-price");
+
+sellPrice.innerHTML = "&#8377;" + (mrpNo - ((mrpNo * discountNo)/100));
+
+count.children[0].onclick = function(){
+    
+}
